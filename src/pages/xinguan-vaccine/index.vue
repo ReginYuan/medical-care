e1adc9ab0ef4b984f56fb7efdeef3e7818560b06
ReginYuan <2496943860@qq.com>
2023-02-01 22:20:01 +0800

    新增新冠疫苗预约

---
+++ src/pages/xinguan-vaccine/index.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="xinguanVaccine">
+		1111
+	</view>
+</template>
+
+<script setup lang="ts">
+</script>
+	
+<style>
+/* 表单提交 */
+@import url('@/common-style/form.css');
+page{
+  background-color: #f7f7f7;
+}
+/* 预约地址 */
+.reserve-address{
+  background-color: #ffffff;
+  padding: 20rpx;
+  margin: 20rpx;
+  border-radius: 10rpx;
+}
+.reserve-address text{
+  display: block;
+}
+.reserve-name{
+  font-weight: bold;
+}
+.reserve-road{
+  padding: 20rpx 0;
+}
+.reserve-title{
+  display: flex;
+  align-items: center;
+}
+.reserve-title text{
+  background-color: #f7f7f7;
+  padding: 5rpx 20rpx;
+  font-size: 25rpx;
+  margin-right: 10rpx;
+}
+.Week{
+  background-color: #ffffff;
+  padding: 20rpx;
+  border-radius: 10rpx;
+  margin: 20rpx;
+  display: flex;
+  align-items: center;
+}
+.Week-flex{
+  display: flex;
+}
+.Week-style{
+  display: flex;
+  flex-direction: column;
+  background: #f7f7f7;
+  text-align: center;
+  padding: 20rpx 0;
+  border-radius: 10rpx;
+  margin-right: 20rpx;
+}
+.Week-style text{
+  padding: 3rpx;
+}
+.Week-sup{
+  width: 150rpx;
+}
+.period-view{
+  background-color: #ffffff;
+  padding: 20rpx;
+  border-radius: 10rpx;
+  margin: 20rpx;
+}
+.period-title{
+  font-weight: bold;
+  margin-bottom: 20rpx;
+  display: block;
+}
+.Week-Down{
+  width: 200rpx;
+}
+/* 选中加上样式 */
+.checkstyle{
+  background-color: #2c76ef !important;
+  color: #ffffff !important;
+}
+</style>
